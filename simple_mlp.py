c5cb4f21d6b866ba3d07d0e0111a53f48897418d
jsapp352 <32817628+jsapp352@users.noreply.github.com>
2019-07-17 01:45:05 -0400

    simple_mlp seems to be working

--- simple_mlp.py
+++ simple_mlp.py
@@ -1,8 +1,14 @@
 # Adapted from code by Milo Spencer-Harper
 # source: https://medium.com/technology-invention-and-more/how-to-build-a-multi-layered-neural-network-in-python-53ec3d1d326a
 
-from numpy import exp, array, random, dot
+import numpy as np
+from numpy import exp, array, random, dot, argmax
 
+# Source: https://medium.com/@awjuliani/simple-softmax-in-python-tutorial-d6b4c4ed5c16
+def softmax(z):
+    z -= np.max(z)
+    sm = (np.exp(z).T / np.sum(np.exp(z),axis=1)).T
+    return sm
 
 class NeuronLayer():
     def __init__(self, number_of_neurons, number_of_inputs_per_neuron):
@@ -10,7 +16,8 @@ class NeuronLayer():
 
 
 class NeuralNetwork():
-    def __init__(self, layer1, layer2):
+    def __init__(self, layer0, layer1, layer2):
+        self.layer0 = layer0
         self.layer1 = layer1
         self.layer2 = layer2
 
@@ -31,7 +38,7 @@ class NeuralNetwork():
     def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
         for iteration in range(number_of_training_iterations):
             # Pass the training set through our neural network
-            output_from_layer_1, output_from_layer_2 = self.think(training_set_inputs)
+            output_from_layer_0, output_from_layer_1, output_from_layer_2 = self.think(training_set_inputs)
 
             # Calculate the error for layer 2 (The difference between the desired output
             # and the predicted output).
@@ -43,25 +50,35 @@ class NeuralNetwork():
             layer1_error = layer2_delta.dot(self.layer2.synaptic_weights.T)
             layer1_delta = layer1_error * self.__sigmoid_derivative(output_from_layer_1)
 
+            # Calculate the error for layer 0 (By looking at the weights in layer 0,
+            # we can determine by how much layer 0 contributed to the error in layer 1).
+            layer0_error = layer1_delta.dot(self.layer1.synaptic_weights.T)
+            layer0_delta = layer0_error * self.__sigmoid_derivative(output_from_layer_0)
+
             # Calculate how much to adjust the weights by
-            layer1_adjustment = training_set_inputs.T.dot(layer1_delta)
+            layer0_adjustment = training_set_inputs.T.dot(layer0_delta)
+            layer1_adjustment = output_from_layer_0.T.dot(layer1_delta)
             layer2_adjustment = output_from_layer_1.T.dot(layer2_delta)
 
             # Adjust the weights.
+            self.layer0.synaptic_weights += layer0_adjustment
             self.layer1.synaptic_weights += layer1_adjustment
             self.layer2.synaptic_weights += layer2_adjustment
 
     # The neural network thinks.
     def think(self, inputs):
-        output_from_layer1 = self.__sigmoid(dot(inputs, self.layer1.synaptic_weights))
+        output_from_layer0 = self.__sigmoid(dot(inputs, self.layer0.synaptic_weights))
+        output_from_layer1 = self.__sigmoid(dot(output_from_layer0, self.layer1.synaptic_weights))
         output_from_layer2 = self.__sigmoid(dot(output_from_layer1, self.layer2.synaptic_weights))
-        return output_from_layer1, output_from_layer2
+        return output_from_layer0, output_from_layer1, output_from_layer2
 
     # The neural network prints its weights
     def print_weights(self):
-        print("    Layer 1 (4 neurons, each with 3 inputs): ")
+        print("    Layer 0 (4 neurons, each with 4 inputs): ")
+        print(self.layer0.synaptic_weights)
+        print("    Layer 1 (4 neurons, each with 4 inputs): ")
         print(self.layer1.synaptic_weights)
-        print("    Layer 2 (1 neuron, with 4 inputs):")
+        print("    Layer 2 (3 neurons, with 4 inputs):")
         print(self.layer2.synaptic_weights)
 
 if __name__ == "__main__":
@@ -69,22 +86,46 @@ if __name__ == "__main__":
     #Seed the random number generator
     random.seed(1)
 
-    # Create layer 1 (4 neurons, each with 3 inputs)
-    layer1 = NeuronLayer(4, 3)
-
-    # Create layer 2 (a single neuron with 4 inputs)
-    layer2 = NeuronLayer(1, 4)
+    # Create neuron layers (M neurons, each with N inputs)
+    layer0 = NeuronLayer(4, 4)
+    layer1 = NeuronLayer(4, 4)
+    layer2 = NeuronLayer(3, 4)
 
     # Combine the layers to create a neural network
-    neural_network = NeuralNetwork(layer1, layer2)
+    neural_network = NeuralNetwork(layer0, layer1, layer2)
 
     print("Stage 1) Random starting synaptic weights: ")
     neural_network.print_weights()
 
     # The training set. We have 7 examples, each consisting of 3 input values
     # and 1 output value.
-    training_set_inputs = array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1], [0, 0, 0]])
-    training_set_outputs = array([[0, 1, 1, 1, 1, 0, 0]]).T
+    training_set_inputs = array(
+        [
+            [1, 0, 1, 0], #V
+            [0, 1, 0, 1], #V
+            [1, 1, 0, 0], #H
+            [0, 0, 1, 1], #H
+            [1, 0, 0, 1], #D
+            [0, 1, 1, 0]  #D
+        ]
+    )
+
+    training_set_outputs = array(
+        [
+            [1, 0, 0], #V
+            [1, 0, 0], #V
+            [0, 1, 0], #H
+            [0, 1, 0], #H
+            [0, 0, 1], #D
+            [0, 0, 1]  #D
+        ]
+    )
+
+    prediction_labels = {
+        0 : "Vertical",
+        1 : "Horizontal",
+        2 : "Diagonal"
+    }
 
     # Train the neural network using the training set.
     # Do it 60,000 times and make small adjustments each time.
@@ -94,6 +135,19 @@ if __name__ == "__main__":
     neural_network.print_weights()
 
     # Test the neural network with a new situation.
-    print("Stage 3) Considering a new situation [1, 1, 0] -> ?: ")
-    hidden_state, output = neural_network.think(array([1, 1, 0]))
-    print(output)
+
+    print("Stage 3) Validation:")
+    for input_set in training_set_inputs:
+        ticks = ["X" if x == 1 else " " for x in input_set]
+
+        hidden_state0, hidden_state1, output = neural_network.think(array(input_set))
+        probs = softmax([output])
+        preds = np.argmax(probs,axis=1)
+
+        print(f"           _______________       Probabilities: {probs}")
+        print(f"          |       |       |      Prediction: {prediction_labels[preds[0]]}")
+        print("          |   {}   |   {}   |".format(ticks[0], ticks[1]))
+        print("          |_______|_______|")
+        print("          |       |       |")
+        print("          |   {}   |   {}   |".format(ticks[2], ticks[3]))
+        print("          |_______|_______|\n\n")
