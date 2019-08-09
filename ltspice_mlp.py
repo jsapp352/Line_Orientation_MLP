f0bad5332ecc7d61c9e0c71aa5e312739a59faf8
jsapp352 <32817628+jsapp352@users.noreply.github.com>
2019-08-09 01:53:19 -0400

    Simulation working with SPICE

--- ltspice_mlp.py
+++ ltspice_mlp.py
@@ -2,6 +2,9 @@
 #  and an LTspice hardware simulation.
 
 import numpy
+import re
+import subprocess
+
 from pprint import pprint
 
 _v_plus = 5
@@ -15,6 +18,10 @@ _pot_tolerance = 0.20
 _r_total_ohms = 5000
 
 _output_filename = 'MLP_netlist.net'
+_result_filename = _output_filename[:-4] + '.log'
+_ltspice_path = '"C:\\Program Files\\LTC\\LTspiceXVII\\XVIIx64.exe" -b '
+
+_simulation_includes_input_buffers = False
 
 class MLP_Circuit_Layer():
     def __init__(self, neuron_layer, layer_number, r_total_ohms):
@@ -22,7 +29,7 @@ class MLP_Circuit_Layer():
         self.neuron_count = neuron_layer.neuron_count
         self.inputs_per_neuron = neuron_layer.inputs_per_neuron
 
-        self.input_nodes = [f'Synapse_{layer_number}_{x}_in' for x in range(0, self.inputs_per_neuron)]
+        self.input_nodes = [f'Synapse_{layer_number}_{x}_in' for x in range(0, self.inputs_per_neuron)] if layer_number == 0 else []
         self.output_nodes = []
 
         self.layer_number = layer_number
@@ -35,7 +42,20 @@ class MLP_Circuit_Layer():
 
     def update_synapse_weights(self):
         #DEV -- Need to add method for randomizing actual resistance/representing pot. tolerance.
-        self.synapses_r_pos = (1 + (self.neuron_layer.synaptic_weights / self.max_weight)) / 2 * self.r_total_ohms
+        max_val = numpy.amax(self.neuron_layer.synaptic_weights)
+        # print(f'max_val: {max_val}')
+        min_val = numpy.amin(self.neuron_layer.synaptic_weights)
+        # print(f'min_val: {min_val}')
+
+        scale = 1 / (max(abs(max_val), abs(min_val)))  # scale weights to take up maximum pot. range
+        # scale = 1 / self.max_weight
+
+        scale *= 0.6
+
+        self.synapses_r_pos = (1 + self.neuron_layer.synaptic_weights * scale) / 2 * self.r_total_ohms
+
+        # self.synapses_r_pos = (1 + (self.neuron_layer.synaptic_weights / self.max_weight)) / 2 * self.r_total_ohms
+
         self.synapses_r_neg = self.r_total_ohms - self.synapses_r_pos
     
     def create_layer_subcircuit(self):
@@ -72,7 +92,7 @@ class MLP_Circuit_Layer():
 
         lines.append(f'XV2_{n_id} Neuron_{n_id}_008 Neuron_{n_id}_004 V+ V- Neuron_{n_id}_005 TL084')
         lines.append(f'XV3_{n_id} 0 Neuron_{n_id}_001 V+ V- {output} TL084')
-        lines.append(f'R**_{n_id} Neuron_{n_id}_005 Neuron_{n_id}_004 100k tol=5')
+        lines.append(f'R**_{n_id} Neuron_{n_id}_005 Neuron_{n_id}_004 50k tol=5')
         lines.append(f'R4_{n_id} Neuron_{n_id}_001 Neuron_{n_id}_005 20k tol=5')
         # lines.append(f'R6_{n_id} Input_{n_id}_008 Neuron_{n_id}_007 5k tol=5')
         lines.append(f'R8_{n_id} Neuron_{n_id}_003 Neuron_{n_id}_001 100k tol=5')
@@ -87,20 +107,24 @@ class MLP_Circuit_Layer():
         return lines
 
     def create_synapse_subcircuit(self, n_id, s_id, input, r_pos, r_neg):
+        simulation_includes_input_buffers = _simulation_includes_input_buffers
+
         lines  = []
 
-        # lines.append(f'XV_buff_{s_id} {input} buff_out_{s_id} V+ V- buff_out_{s_id} TL084')
-        # lines.append(f'XV_buff`_{s_id} 0 buff`_inv_{s_id} V+ V- buff`_out_{s_id} TL084')
-        # lines.append(f'R1_buff`_{s_id} {input} buff`_inv_{s_id} 100k')
-        # lines.append(f'R2_buff`_{s_id} buff`_inv_{s_id} buff_out_{s_id} 100k')
+        if simulation_includes_input_buffers:
+            lines.append(f'XV_buff_{s_id} {input} buff_out_{s_id} V+ V- buff_out_{s_id} TL084')
+            lines.append(f'XV_buff`_{s_id} 0 buff`_inv_{s_id} V+ V- buff`_out_{s_id} TL084')
+            lines.append(f'R1_buff`_{s_id} {input} buff`_inv_{s_id} 100k')
+            lines.append(f'R2_buff`_{s_id} buff`_inv_{s_id} buff_out_{s_id} 100k')
 
-        # lines.append(f'R_in_{s_id} buff_out_{s_id} Input_{s_id} {r_pos}')
-        # lines.append(f'R_in`_{s_id} buff`_out_{s_id} Input_{s_id} {r_neg}')
-        # lines.append(f'R_in_{s_id}_series Input_{s_id} Neuron_{n_id}_008 {self.r_total_ohms}')
+            lines.append(f'R_in_{s_id} buff_out_{s_id} Input_{s_id} {r_pos}')
+            lines.append(f'R_in`_{s_id} buff`_out_{s_id} Input_{s_id} {r_neg}')
+            lines.append(f'R_in_{s_id}_series Input_{s_id} Neuron_{n_id}_008 {self.r_total_ohms}')
+        else:
+            lines.append(f'R_in_{s_id} {input} Input_{s_id} {r_pos}')
+            lines.append(f'R_in`_{s_id} {input}` Input_{s_id} {r_neg}')
+            lines.append(f'R_in_{s_id}_series Input_{s_id} Neuron_{n_id}_008 {self.r_total_ohms}')
 
-        lines.append(f'R_in_{s_id} {input} Input_{s_id} {r_pos}')
-        lines.append(f'R_in`_{s_id} {input}` Input_{s_id} {r_neg}')
-        lines.append(f'R_in_{s_id}_series Input_{s_id} Neuron_{n_id}_008 {self.r_total_ohms}')
         lines.append('\n')
 
         return lines
@@ -116,6 +140,8 @@ class MLP_Circuit():
         self.r_total_ohms = _r_total_ohms
         
         self.output_filename = _output_filename
+        self.result_filename = _result_filename
+        self.ltspice_path = _ltspice_path
         
         self.neural_network = neural_network
         self.hardware_layers = []
@@ -126,6 +152,40 @@ class MLP_Circuit():
         self.power_sources['V-'] = _v_minus
 
         self.input_sources = []
+    
+    def initialize_hardware_layers(self):
+        for idx,model_layer in enumerate(self.neural_network.neuron_layers):
+            self.hardware_layers.append(MLP_Circuit_Layer(model_layer, idx, self.r_total_ohms))
+        
+        for i in range(1, len(self.hardware_layers)):
+            self.hardware_layers[i].input_nodes = self.hardware_layers[i-1].output_nodes
+
+
+    def update_input_sources(self, input_array):
+        # print('Update_input_sources() input_array:')
+        # pprint(input_array)
+        # print('\n')
+
+        inputs = input_array.tolist()
+
+        # print('Update_input_sources() inputs.tolist():')
+        # pprint(inputs)
+        # print('\n')
+
+        # pprint(inputs)
+
+        min_input = min(inputs)
+        max_input = max(inputs)
+
+        input_range = abs(max_input - min_input)
+        v_in_range = self.v_in_max - self.v_in_min
+        map_scale_factor = v_in_range / input_range
+        
+        map_input = lambda x: (x - min_input) * map_scale_factor + self.v_in_min
+
+        self.input_sources = [map_input(x) for x in inputs]
+
+        # pprint(self.input_sources)
 
     def create_header(self):
         lines = []
@@ -142,10 +202,16 @@ class MLP_Circuit():
     def create_footer(self):
         lines = []
         
-        # lines.append('.tran 0 110p 100p')
-        # lines.append('.option plotwinsize=0 numdgt=6')
-        lines.append('.op')
-        lines.append('.backanno')
+        lines.append('.tran 0 110p 100p')
+        lines.append('.option plotwinsize=0')
+
+        # Skip N-R iterative solving
+        lines.append('.options noopiter')
+
+        # Skip gmin stepping
+        lines.append('.options gminsteps=0')
+
+        # lines.append('.op')
         lines.append('.end')
 
         lines.append('\n')
@@ -159,50 +225,37 @@ class MLP_Circuit():
             for node in layer.output_nodes:
                 lines.append(f'.save V({node})')
 
-        # for layer in self.hardware_layers:
-        #     for node in layer.output_nodes:
-        #         lines.append(f'.measure V{node} avg V({node})')
+        for layer in self.hardware_layers:
+            for node in layer.output_nodes:
+                lines.append(f'.measure V{node} avg V({node})')
         
         lines.append('\n')
 
         return lines
     
     def create_source_definitions(self):
+        simulation_includes_input_buffers = _simulation_includes_input_buffers
+
         lines = []
         i = 0
 
         for node in self.power_sources:
-            lines.append(f'V{i} {node} 0 {self.power_sources[node]}V')
+            lines.append(f'V{i} {node} 0 {self.power_sources[node]}V Rser=0.1')
             i += 1
         
         lines.append('\n')
 
         for idx,voltage in enumerate(self.input_sources):
             node = self.hardware_layers[0].input_nodes[idx]
-            lines.append(f'V{i} {node} 0 {voltage}')
-            lines.append(f'V{i}` {node}` 0 {voltage * (-1)}')
+            lines.append(f'V{i} {node} 0 {voltage} Rser=0.1')
+            
+            if not simulation_includes_input_buffers:
+                lines.append(f'V{i}` {node}` 0 {voltage * (-1)} Rser=0.1')
             i += 1
 
         lines.append('\n')
 
         return lines
-    
-    def update_input_sources(self, inputs):
-        min_input = min(inputs)
-        max_input = max(inputs)
-
-        input_range = max_input - min_input
-        v_in_range = self.v_in_max - self.v_in_min
-        map_scale_factor = v_in_range / input_range
-        
-        map_input = lambda x: (x - min_input) * map_scale_factor + self.v_in_min
-
-        self.input_sources = [map_input(x) for x in inputs]
-
-    
-    def initialize_hardware_layers(self):
-        for idx,model_layer in enumerate(self.neural_network.neuron_layers):
-            self.hardware_layers.append(MLP_Circuit_Layer(model_layer, idx, self.r_total_ohms))
     
     def create_netlist(self):
         lines = []
@@ -220,6 +273,56 @@ class MLP_Circuit():
             for line in lines:
                 f.write(line)
                 f.write('\n')
+
+    def run_simulation(self):
+        command_string = self.ltspice_path + self.output_filename
+        subprocess.check_call(command_string)
+
+    def get_outputs(self):
+        outputs = []
+
+        with open(self.result_filename, 'r') as f:
+            result = f.read()
+            
+            for layer in self.hardware_layers:
+                output_voltages = []
+
+                for node in layer.output_nodes:
+                    pattern = re.compile(f'({node.lower()}\)\)\=)(-?\d+\.\d+)(\s)')
+                    output_voltages.append(float(pattern.search(result)[2]))
+
+                outputs.append((numpy.asarray(output_voltages) + self.v_in_max) / (2 * self.v_in_max))
+                # pprint(outputs[-1])
+
+        # pprint(numpy.asarray(outputs))
+        return numpy.asarray(outputs)
+
+    def think(self, inputs):
+        # print('Ckt.Think() inputs:')
+        # pprint(inputs)
+        # print('\n')
+        # print('Ckt.Think() inputs.tolist():')
+        # pprint(inputs.tolist())
+        # print('\n')
+
+        self.update_input_sources(inputs)
+
+        for layer in self.hardware_layers:
+            layer.update_synapse_weights()
+        
+        self.create_netlist()
+        self.run_simulation()
+        
+        outputs = self.get_outputs()
+
+        print(outputs)
+
+        return outputs
+
+
+
+
+
 
 
     
